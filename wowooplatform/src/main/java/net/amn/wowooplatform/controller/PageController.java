ef7075aaca771749ab32da557190d349a10e55e7
Amin Roslan <aminroslan1994@gmail.com>
2018-10-04 12:32:36 +0800

    first frontend setup

--- wowooplatform/src/main/java/net/amn/wowooplatform/controller/PageController.java
+++ wowooplatform/src/main/java/net/amn/wowooplatform/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.amn.wowooplatform.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,5 +16,25 @@ public class PageController {
 		return mv;
 		
 	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting == null) {
+//			greeting="Hello There";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+//	
+//	@RequestMapping(value="/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting")String greeting) {
+//		if(greeting == null) {
+//			greeting="Hello There";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
 	
 }
